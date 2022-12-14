53cd181316f08cdd54f0e3a77a5de39a5760cabc
jcrbuilds <80365426+jcrbuilds@users.noreply.github.com>
2022-12-14 15:35:45 +0000

    Final push ima do rn

--- src/scripts/Searchdb.js
+++ src/scripts/Searchdb.js
@@ -1,6 +1,12 @@
 let jsonData = `[
     {"Name": "FullSet Scania", "Des": "https://cdn.discordapp.com/attachments/1032017862027657306/1050741169119821874/Low_Spoiler_Scania.txt", "Image": "https://cdn.discordapp.com/attachments/1032017862027657306/1050741169543462992/ets2_20221120_192654_00.png"},
     {"Name": "FullSet Scania", "Des": "https://cdn.discordapp.com/attachments/1032017862027657306/1045403077877973052/Coles__Sons_Custom_S580.txt", "Image": "https://media.discordapp.net/attachments/1032017862027657306/1045403077471109202/ets2_20221124_180125_00.png?width=1214&height=683"},
+    {"Name": "Name here", "Des": "Code", "Image": "https://cdn.discordapp.com/attachments/1032017862027657306/1050741169543462992/ets2_20221120_192654_00.png"},
+    {"Name": "Name here", "Des": "Code", "Image": "https://cdn.discordapp.com/attachments/1032017862027657306/1050741169543462992/ets2_20221120_192654_00.png"},
+    {"Name": "Name here", "Des": "Code", "Image": "https://cdn.discordapp.com/attachments/1032017862027657306/1050741169543462992/ets2_20221120_192654_00.png"},
+    {"Name": "Name here", "Des": "Code", "Image": "https://cdn.discordapp.com/attachments/1032017862027657306/1050741169543462992/ets2_20221120_192654_00.png"},
+    {"Name": "Name here", "Des": "Code", "Image": "https://cdn.discordapp.com/attachments/1032017862027657306/1050741169543462992/ets2_20221120_192654_00.png"},
+    {"Name": "Name here", "Des": "Code", "Image": "https://cdn.discordapp.com/attachments/1032017862027657306/1050741169543462992/ets2_20221120_192654_00.png"},
     {"Name": "Name here", "Des": "Code", "Image": "https://cdn.discordapp.com/attachments/1032017862027657306/1050741169543462992/ets2_20221120_192654_00.png"}
   ]`
   
