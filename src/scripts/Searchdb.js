6762ceec6a3285db13a2e550bf640f3f8d99189b
jcrbuilds <80365426+jcrbuilds@users.noreply.github.com>
2022-12-14 01:02:55 +0000

    fix

--- src/scripts/Searchdb.js
+++ src/scripts/Searchdb.js
@@ -1,10 +1,11 @@
 let jsonData = `[
+    {"Name": "Code Here", "req": "Requirements Here", "Image": "https://cdn.discordapp.com/attachments/1032017862027657306/1050741169543462992/ets2_20221120_192654_00.png"},
     {"Name": "Code Here", "req": "Requirements Here", "Image": "https://cdn.discordapp.com/attachments/1032017862027657306/1050741169543462992/ets2_20221120_192654_00.png"}
   ]`
   
   let data = JSON.parse(jsonData)
   
-  function search_animal() {
+  function search_code() {
     let input = document.getElementById('searchbar').value
     input = input.toLowerCase();
     let x = document.querySelector('#list-holder');
