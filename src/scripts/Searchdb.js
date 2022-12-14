b7a52c8f0299248358bd2de05bbf845dd9c87d2a
jcrbuilds <80365426+jcrbuilds@users.noreply.github.com>
2022-12-14 15:24:13 +0000

    save edit stuff

--- src/scripts/Searchdb.js
+++ src/scripts/Searchdb.js
@@ -1,7 +1,7 @@
 let jsonData = `[
-    {"Name": "Code Here", "req": "Requirements Here", "Image": "https://cdn.discordapp.com/attachments/1032017862027657306/1050741169543462992/ets2_20221120_192654_00.png"},
-    {"Name": "Code Here", "req": "Requirements Here", "Image": "https://cdn.discordapp.com/attachments/1032017862027657306/1050741169543462992/ets2_20221120_192654_00.png"},
-    {"Name": "Code Here", "req": "Requirements Here", "Image": "https://cdn.discordapp.com/attachments/1032017862027657306/1050741169543462992/ets2_20221120_192654_00.png"}
+    {"Name": "FullSet Scania", "Des": "https://cdn.discordapp.com/attachments/1032017862027657306/1050741169119821874/Low_Spoiler_Scania.txt", "Image": "https://cdn.discordapp.com/attachments/1032017862027657306/1050741169543462992/ets2_20221120_192654_00.png"},
+    {"Name": "FullSet Scania", "Des": "https://cdn.discordapp.com/attachments/1032017862027657306/1045403077877973052/Coles__Sons_Custom_S580.txt", "Image": "https://media.discordapp.net/attachments/1032017862027657306/1045403077471109202/ets2_20221124_180125_00.png?width=1214&height=683"},
+    {"Name": "Name here", "Des": "Code", "Image": "https://cdn.discordapp.com/attachments/1032017862027657306/1050741169543462992/ets2_20221120_192654_00.png"}
   ]`
   
   let data = JSON.parse(jsonData)
@@ -20,7 +20,7 @@ let jsonData = `[
         elem.innerHTML = `
         <div class="Codecont">
         <img src="${obj.Image}"> <p>
-        ${obj.Name} </p> ${obj.req}
+        <h1> ${obj.Name} </h1> </p> <a href="${obj.Des}"><i class="fa fa-download"></i> Download</a>
         </div>`
         x.appendChild(elem)
       }
