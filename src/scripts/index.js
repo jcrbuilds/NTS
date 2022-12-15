8972b3af129948eca26da9ff41b6c0922afd9f3b
CookieGranate <cookiesooslol@gmail.com>
2022-12-15 21:51:01 +0100

    struggeling with sidebar, index.html & navbar.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,16 +4,3 @@
 
 // }
 
-var sidebarEl = document.getElementById('sidebar')
-var toggleBtn = document.getElementById('opennav')
-
-function toggleSidebar() {
-    var sidebarEl = document.getElementById('sidebarmain');
-    var toggleBtn = document.getElementsByName('opennav');
-
-    if (sidebarEl.style.width === '200px') {
-            sidebarEl.style.width = '0'
-        } else {
-            sidebarEl.style.width = '200px'
-        }
-}
